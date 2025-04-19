8cacb483abae5643dc38c862deeae3708ec02872
darrasHumber <n01546480@humber.ca>
2025-04-18 21:46:14 -0400

    Test Perishable Product class

--- script.js
+++ script.js
@@ -11,14 +11,17 @@ class Product {
   }
   //print the Product in a nice way
   toString() {
-    return `Product: ${this.name}, Price: ${this.price},Quantity: ${this.quantity}`;
+    return `Product: ${this.name}, Price: $${this.price},Quantity: ${this.quantity}`;
   }
 }
 
 console.log("Test Product Class\n\n");
 const galaApple = new Product("Apple", 1.99, 5);
-console.log(galaApple.getValue());
+
 console.log(galaApple.toString());
+console.log(
+  `${galaApple.name} subtotal is $${galaApple.getValue().toFixed(2)}`
+);
 
 // Part 2: Adding Inheritance
 class PerishableProduct extends Product {
@@ -27,6 +30,15 @@ class PerishableProduct extends Product {
     this.expirationDate = expirationDate;
   }
   toString() {
-    return `Product: ${this.name}, Price: ${this.price},Quantity: ${this.quantity}, Expiry Date: ${this.expirationDate}`;
+    return `Product: ${this.name}, Price: $${this.price},Quantity: ${this.quantity}, Expiry Date: ${this.expirationDate}`;
   }
 }
+
+// Test PerishableProduc class
+const milk = new PerishableProduct("Milk", 6.99, 2, "20=04-2025");
+const bread = new PerishableProduct("bread", 2.99, 3, "28=04-2025");
+console.log("\nTest PerishableProduct class\n\n");
+console.log(milk.toString());
+console.log(`${milk.name} subtotal is $${milk.getValue().toFixed(2)}`);
+console.log(bread.toString());
+console.log(`${bread.name} subtotal is $${bread.getValue().toFixed(2)}`);
